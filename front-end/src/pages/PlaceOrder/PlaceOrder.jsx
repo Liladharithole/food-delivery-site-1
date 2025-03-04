d857c03edb2148f8b22378ebabcfe24863f901b3
LILADHAR <97004436+Liladharithole@users.noreply.github.com>
2025-03-04 10:56:45 +0530

    Updating add to cart and checkout page

--- front-end/src/pages/PlaceOrder/PlaceOrder.jsx
+++ front-end/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,8 +1,54 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./PlaceOrder.css";
+import { StoreContext } from "../../context/StoreContext";
 
 const PlaceOrder = () => {
-  return <div>PlaceOrder</div>;
+
+  const {getTotalCartAmount} =  useContext(StoreContext);
+  return (
+    <form className="place-order" id="place-order">
+      <div className="place-order-left">
+        <p className="title">Delivery Information</p>
+        <div className="multi-fields">
+          <input type="text" name="" id="" placeholder="Frist Name" />
+          <input type="text" name="" id="" placeholder="Last Name" />
+        </div>
+        <input type="email" placeholder="Email" />
+        <input type="text" placeholder="Street" />
+        <div className="multi-fields">
+          <input type="text" name="" id="" placeholder="City" />
+          <input type="text" name="" id="" placeholder="State" />
+        </div>
+        <div className="multi-fields">
+          <input type="text" name="" id="" placeholder="Zip code" />
+          <input type="text" name="" id="" placeholder="Country" />
+        </div>
+        <input type="text" placeholder="Phone" />
+      </div>
+      <div className="place-order-right">
+      <div className="cart-total">
+          <h2>Cart Totals</h2>
+          <div className="cart-total-details">
+              <p>Subtotal</p>
+              <p>₹{getTotalCartAmount()}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <p>Shipping</p>
+              <p>₹{getTotalCartAmount() === 0 ? 0 : 20}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <b>Total</b>
+              <b>
+                ₹{getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 20}
+              </b>
+            </div>
+          <button>PROCEED TO PAYMENT</button>
+        </div>
+      </div>
+    </form>
+  );
 };
 
 export default PlaceOrder;
